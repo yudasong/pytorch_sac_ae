fa154d94c93181f3a445191d29a3b1717c5a41ce
yudasong <anepycn@gmail.com>
2021-08-12 03:47:03 -0400

    2Q

--- sac_transfer.py
+++ sac_transfer.py
@@ -239,6 +239,11 @@ class SacTransferAgent(object):
 
         self.critic_target.load_state_dict(self.critic.state_dict())
 
+        self.ag_critic = Critic(
+            obs_shape, action_shape, hidden_dim, encoder_type,
+            encoder_feature_dim, num_layers, num_filters
+        ).to(device)
+
         # tie encoders between actor and critic
         self.actor.encoder.copy_conv_weights_from(self.critic.encoder)
 
@@ -256,6 +261,10 @@ class SacTransferAgent(object):
             self.critic.parameters(), lr=critic_lr, betas=(critic_beta, 0.999)
         )
 
+        self.ag_critic_optimizer = torch.optim.Adam(
+            self.ag_critic.parameters(), lr=critic_lr, betas=(critic_beta, 0.999)
+        )
+
         self.log_alpha_optimizer = torch.optim.Adam(
             [self.log_alpha], lr=alpha_lr, betas=(alpha_beta, 0.999)
         )
@@ -267,6 +276,7 @@ class SacTransferAgent(object):
         self.training = training
         self.actor.train(training)
         self.critic.train(training)
+        self.ag_critic.train(training)
 
     @property
     def alpha(self):
@@ -299,17 +309,7 @@ class SacTransferAgent(object):
             target_Q1, target_Q2 = self.critic_target(next_obs, policy_action)
             target_V = torch.min(target_Q1,target_Q2) - self.alpha.detach() * log_pi
             target_Q = reward + (not_done * self.discount * target_V)
-            '''
-            target_V = torch.zeros(len(obs),1).to(self.device)
-            for i in range(10):
-                _, policy_action, log_pi, _ = expert.actor(next_obs)
-                target_Q1, target_Q2 = expert.critic_target(next_obs, policy_action)
-                target_V = target_V + torch.min(target_Q1,
-                                 target_Q2) - expert.alpha.detach() * log_pi
-            target_V = target_V / 10
-            '''
             
-             
             #target_V = bc_agent.value_net(next_obs)
             target_Q = reward + (not_done * self.discount * target_V)
                          
@@ -328,13 +328,44 @@ class SacTransferAgent(object):
 
         self.critic.log(L, step)
 
+    def update_ag_critic(self, bc_agent, expert, obs, action, reward, next_obs, not_done, L, step):
+        with torch.no_grad():
+            
+            
+            
+            target_V = torch.zeros(len(obs),1).to(self.device)
+            for i in range(10):
+                _, policy_action, log_pi, _ = expert.actor(next_obs)
+                target_Q1, target_Q2 = expert.critic_target(next_obs, policy_action)
+                target_V = target_V + torch.min(target_Q1,
+                                 target_Q2) - expert.alpha.detach() * log_pi
+            target_V = target_V / 10            
+             
+            #target_V = bc_agent.value_net(next_obs)
+            target_Q = reward + (not_done * self.discount * target_V)
+                         
+        # get current Q estimates
+        #current_Q1, current_Q2 = self.critic(obs, action)
+        current_Q1, current_Q2 = self.ag_critic(obs, action, detach_encoder=False)
+        critic_loss = F.mse_loss(current_Q1,
+                                 target_Q) + F.mse_loss(current_Q2, target_Q)
+        #L.log('train_critic/loss', critic_loss, step)
+        # Optimize the critic
+        self.ag_critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.ag_critic_optimizer.step()
+
+        self.ag_critic.log(L, step)
+
     def update_actor_and_alpha(self, obs, L, step):
         # detach encoder, so we don't update it with the actor loss
         _, pi, log_pi, log_std = self.actor(obs, detach_encoder=True)
         actor_Q1, actor_Q2 = self.critic(obs, pi, detach_encoder=True)
-
+        ag_Q1, ag_Q2 = self.ag_critic(obs, pi, detach_encoder=True)
         actor_Q = torch.min(actor_Q1, actor_Q2)
-        actor_loss = (self.alpha.detach() * log_pi - actor_Q).mean()
+        ag_Q = torch.min(ag_Q1,ag_Q2)
+        Q = 0.5 * actor_Q + 0.5 * ag_Q
+        actor_loss = (self.alpha.detach() * log_pi - Q).mean()
 
         L.log('train_actor/loss', actor_loss, step)
         L.log('train_actor/target_entropy', self.target_entropy, step)
@@ -364,8 +395,10 @@ class SacTransferAgent(object):
 
         if self.encoder_type == 'identity':
             self.update_critic(bc_agent, expert, state, action, reward, next_state, not_done, L, step)
+            self.update_ag_critic(bc_agent, expert, state, action, reward, next_state, not_done, L, step)
         else:
             self.update_critic(bc_agent, expert, obs, action, reward, next_obs, not_done, L, step)
+            self.update_ag_critic(bc_agent, expert, obs, action, reward, next_obs, not_done, L, step)
 
         if step % self.actor_update_freq == 0:
             if self.encoder_type == 'identity':
@@ -391,6 +424,9 @@ class SacTransferAgent(object):
         torch.save(
             self.critic.state_dict(), '%s/critic_%s.pt' % (model_dir, step)
         )
+        torch.save(
+            self.ag_critic.state_dict(), '%s/ag_critic_%s.pt' % (model_dir, step)
+        )
 
     def load(self, model_dir, step):
         self.actor.load_state_dict(
@@ -400,5 +436,8 @@ class SacTransferAgent(object):
             torch.load('%s/critic_%s.pt' % (model_dir, step))
         )
         self.critic_target.load_state_dict(self.critic.state_dict())
+        self.ag_critic.load_state_dict(
+            torch.load('%s/critic_%s.pt' % (model_dir, step))
+        )
         #self.actor.encoder.copy_conv_weights_from(self.critic.encoder)
         self.log_alpha.data.copy_(torch.log(torch.load('%s/alpha_%s.pt' % (model_dir, step))))
